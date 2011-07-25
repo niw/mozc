4dc038a1e655e24eec9f9fdd1f2469b9964ea4f4
Yoshimasa Niwa <niw@niw.at>
2011-07-25 04:06:30 -0700

    Add a script to checkout dependencies from svn repositories.

---
+++ checkout_externals.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+svn checkout http://protobuf.googlecode.com/svn/trunk@375 src/protobuf/files
+svn checkout http://gyp.googlecode.com/svn/trunk@922 src/third_party/gyp
+svn checkout https://zinnia.svn.sourceforge.net/svnroot/zinnia/zinnia@16 src/third_party/zinnia/v0_04
+svn checkout http://googletest.googlecode.com/svn/trunk@484 src/third_party/gtest
