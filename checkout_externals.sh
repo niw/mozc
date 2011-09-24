42009859df07257df4f95c2547a02ae94e0b00d7
Yoshimasa Niwa <niw@niw.at>
2011-09-24 00:09:40 -0700

    Update helper script to update dependencies.

--- checkout_externals.sh
+++ checkout_externals.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-svn checkout http://protobuf.googlecode.com/svn/trunk@375 src/protobuf/files
-svn checkout http://gyp.googlecode.com/svn/trunk@922 src/third_party/gyp
-svn checkout https://zinnia.svn.sourceforge.net/svnroot/zinnia/zinnia@16 src/third_party/zinnia/v0_04
-svn checkout http://googletest.googlecode.com/svn/trunk@484 src/third_party/gtest
+rm -rf src/protobuf/files; svn checkout http://protobuf.googlecode.com/svn/trunk@375 src/protobuf/files
+rm -rf src/third_party/gtest; svn checkout http://googletest.googlecode.com/svn/trunk@484 src/third_party/gtest
+rm -rf src/third_party/gyp; svn checkout http://gyp.googlecode.com/svn/trunk@937 src/third_party/gyp
+rm -rf src/third_party/japanese_usage_dictionary; svn checkout http://japanese-usage-dictionary.googlecode.com/svn/trunk@3 src/third_party/japanese_usage_dictionary
+rm -rf src/third_party/zinnia/v0_04; svn checkout https://zinnia.svn.sourceforge.net/svnroot/zinnia/zinnia@16 src/third_party/zinnia/v0_04
