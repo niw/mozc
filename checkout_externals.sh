95edea00995b121895b701959b6994b0be099f50
Yoshimasa Niwa <niw@niw.at>
2012-03-13 00:50:05 -0700

    Sync a helper script.

--- checkout_externals.sh
+++ checkout_externals.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 rm -rf src/protobuf/files; svn checkout http://protobuf.googlecode.com/svn/trunk@375 src/protobuf/files
-rm -rf src/third_party/gtest; svn checkout http://googletest.googlecode.com/svn/trunk@484 src/third_party/gtest
-rm -rf src/third_party/gyp; svn checkout http://gyp.googlecode.com/svn/trunk@1034 src/third_party/gyp
+rm -rf src/third_party/gmock; svn checkout http://googlemock.googlecode.com/svn/trunk@403 src/third_party/gmock
+rm -rf src/third_party/gtest; svn checkout http://googletest.googlecode.com/svn/trunk@605 src/third_party/gtest
+rm -rf src/third_party/gyp; svn checkout http://gyp.googlecode.com/svn/trunk@1167 src/third_party/gyp
 rm -rf src/third_party/jsoncpp; svn checkout http://jsoncpp.svn.sourceforge.net/svnroot/jsoncpp/trunk/jsoncpp@230 src/third_party/jsoncpp
 rm -rf src/third_party/zinnia/v0_04; svn checkout https://zinnia.svn.sourceforge.net/svnroot/zinnia/zinnia@16 src/third_party/zinnia/v0_04
