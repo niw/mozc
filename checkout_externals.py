65c0400e1fba798d8934cb0f0d04f91a97fc1343
Yoshimasa Niwa <niw@niw.at>
2014-03-30 16:15:20 -0700

    Support File() command to checkout a single file.

--- checkout_externals.py
+++ checkout_externals.py
@@ -12,10 +12,31 @@ deps_os = {}
 def Var(name):
   return vars[name]
 
-def checkout(dir, url, force=False):
+class FileImpl(object):
+  def __init__(self, file_location):
+    self.file_location = file_location
+
+  def __str__(self):
+    return self.file_location
+
+  def GetPath(self):
+    return os.path.split(self.file_location)[0]
+
+  def GetFilename(self):
+      rev_tokens = self.file_location.split('@')
+      return os.path.split(rev_tokens[0])[1]
+
+def File(file_location):
+  return FileImpl(file_location)
+
+def checkout(dir, item, force=False):
   if force:
     shutil.rmtree(dir, True)
-  os.system("svn checkout --trust-server-cert --non-interactive %s %s" % (url, dir))
+  if isinstance(item, FileImpl):
+    os.system("svn checkout --trust-server-cert --non-interactive --depth empty %s %s" % (item.GetPath(), dir))
+    os.system("svn update --trust-server-cert --non-interactive %s" % os.path.join(dir, item.GetFilename()))
+  else:
+    os.system("svn checkout --trust-server-cert --non-interactive %s %s" % (item, dir))
 
 def parse_args():
   parser = optparse.OptionParser(usage="%prog [options] {" + ", ".join(deps_os.keys()) + "}")
@@ -29,10 +50,10 @@ def main():
   (options, args) = parse_args()
 
   if len(args) > 0 and deps_os.has_key(args[0]):
-    for dir, url in deps.iteritems():
-      checkout(dir, url, options.force)
-    for dir, url in deps_os[args[0]].iteritems():
-      checkout(dir, url, options.force)
+    for dir, item in deps.iteritems():
+      checkout(dir, item, options.force)
+    for dir, item in deps_os[args[0]].iteritems():
+      checkout(dir, item, options.force)
   else:
     print >> sys.stderr, "unknown or missing platform."
     exit(1)
